8331cf76002199c4312a7799d118d62a08ae6ac1
linchengzzz <1139489076@qq.com>
2018-09-17 00:34:11 +0800

    定义猜谜游戏

---
+++ src/2.字母游戏.js
@@ -0,0 +1,9 @@
+// 定义字典模板
+const arr = [
+    ['t', 'h', 'i', 's'],
+    ['w', 'a', 't', 's'],
+    ['o', 'a', 'h', 'g'],
+    ['f', 'g', 'd', 't']
+];
+// 定义单词
+const strArr = ['this', 'two', 'fat', 'that'];
