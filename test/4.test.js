40292f3f8a5d49405b21510d3143ff7a09d8e2bd
linchengzzz <1139489076@qq.com>
2018-09-20 18:49:52 +0800

    提交测试

---
+++ test/4.test.js
@@ -0,0 +1,11 @@
+const { fib1, fib2 } = require('../src/4.递归测试');
+const expect = require('chai').expect;
+
+describe('递归测试', () => {
+    it('fib1', () => {
+        expect(fib1(40)).to.be.an('number');
+    });
+    it('fib2', () => {
+        expect(fib2(40)).to.be.an('number');
+    });
+})
