0f7c9c9a93dee5741258de766e182b9fccd6d61a
linchengzzz <1139489076@qq.com>
2018-09-21 12:09:12 +0800

    线性算法

--- src/5.最大子序列.js
+++ src/5.最大子序列.js
@@ -1,3 +1,4 @@
+// 穷举法求最大子序列和 n3算法
 const maxSubSum = (arr) => {
     let maxSum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -14,21 +15,72 @@ const maxSubSum = (arr) => {
     return maxSum;
 };
 
+// 优化后的迭代求最大子序列和--n2算法
 const maxSubSum2 = (arr) => {
     let maxSum = 0;
     for (let i = 0; i < arr.length; i++) {
         let curSum = 0;
         for (let j = i; j < arr.length; j++) {
             curSum += arr[j];
-            if(curSum > maxSum) {
+            if (curSum > maxSum) {
                 maxSum = curSum;
             }
         }
     }
     return maxSum;
-}
+};
+
+// 分治法求最大子序列和--NlogN算法
+const maxSubSum3 = (arr, left, right) => {
+    // 初始化递归变量
+    left === undefined ? (left = 0) : left;
+    right === undefined ? (right = arr.length - 1) : right;
+    if (left >= right) {
+        return arr[left] > 0 ? arr[left] : 0;
+    }
+    // 分治递归
+    const center = parseInt((left + right) / 2);
+    const leftMaxSum = maxSubSum3(arr, left, center);
+    const rightMaxSum = maxSubSum3(arr, center + 1, right);
+
+    // 求左序列最大值
+    let leftBorderSum = 0;
+    let leftBorderMaxSum = 0;
+    for (let i = center; i >= left; i--) {
+        leftBorderSum += arr[i];
+        if (leftBorderSum > leftBorderMaxSum) {
+            leftBorderMaxSum = leftBorderSum;
+        }
+    }
+    // 求右序列最大值
+    let rightBorderSum = 0;
+    let rightBorderMaxSum = 0;
+    for (let i = center + 1; i <= right; i++) {
+        rightBorderSum += arr[i];
+        if (rightBorderSum > rightBorderMaxSum) {
+            rightBorderMaxSum = rightBorderSum;
+        }
+    }
+    // 返回左序列 或 右序列 或 连续序列 的 最大值\
+    return Math.max(leftMaxSum, rightMaxSum, leftBorderMaxSum + rightBorderMaxSum);
+};
+const maxSubSum4 = (arr) => {
+    let sum = 0;
+    let maxSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        if (sum > maxSum) {
+            maxSum = sum;
+        } else if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxSum;
+};
 
 module.exports = {
     maxSubSum,
     maxSubSum2,
-}
+    maxSubSum3,
+    maxSubSum4,
+};
--- test/5.test.js
+++ test/5.test.js
@@ -1,26 +1,56 @@
-const { maxSubSum, maxSubSum2 } =require('../src/5.最大子序列');
+const { maxSubSum, maxSubSum2, maxSubSum3, maxSubSum4 } = require('../src/5.最大子序列');
 const expect = require('chai').expect;
 
 /**
- * Test Result:
- *   num: 2000
- *   ✓ 最大子序列和 n3 算法: 1646ms
- *   ✓ 最大子序列和 n2 算法: 6ms
+ * Test1 Result:
+ *   maxNum: 2000
+ *   ✓ 最大子序列和 穷举算法 => n3时间复杂度 Time: 1646ms
+ *   ✓ 最大子序列和 穷举算法 => n3时间复杂度 Time: 6ms
+ * 
  *   2 passing (2s)
  */
-describe('最大子序列和', ()=> {
-    const maxNum = 2000;
+
+/**
+ * Test2 Result:
+ *   maxNum: 100000
+ *   - 最大子序列和 穷举算法 => n3时间复杂度 Time: NA
+ *   ✓ 最大子序列和 优化穷举算法 => n2时间复杂度 Time: 8855ms
+ *   ✓ 最大子序列和 分治递归算法 => nlogn时间复杂度 Time: 18ms
+ * 
+ *   2 passing (9s)
+ *   1 pending
+ */
+
+/**
+ * Test3 Result:
+ *   maxNum: 10000000
+ *   - 最大子序列和 穷举算法 => O(n3)时间复杂度 Time: NA
+ *   - 最大子序列和 优化穷举算法 => O(n2)时间复杂度 Time: NA
+ *   ✓ 最大子序列和 分治递归算法 => O(nlogn)时间复杂度 Time: 1759ms
+ *   ✓ 最大子序列和 线性算法 => O(n)时间复杂度 Time: 73ms
+ * 
+ *   2 passing (2s)
+ *   2 pending
+ */
+describe('最大子序列和', () => {
+    const maxNum = 10000000;
     const arr = [];
     before(() => {
         //  产生 maxNum 个 随机数 正数 > 负数
         for (let i = 0; i < maxNum; i++) {
-            arr.push(Math.round(7000 - Math.random()*10000));
+            arr.push(Math.round(7000 - Math.random() * 10000));
         }
     });
-    it('n3 算法',()=> {
+    it.skip('穷举算法 => O(n3)时间复杂度 Time', () => {
         expect(maxSubSum(arr)).to.be.an('number');
     });
-    it('n2 算法',()=> {
+    it.skip('优化穷举算法 => O(n2)时间复杂度 Time', () => {
         expect(maxSubSum2(arr)).to.be.an('number');
-    })
-})
+    });
+    it('分治递归算法 => O(nlogn)时间复杂度 Time', () => {
+        expect(maxSubSum3(arr)).to.be.an('number');
+    });
+    it('线性算法 => O(n)时间复杂度 Time', () => {
+        expect(maxSubSum4(arr)).to.be.an('number');
+    });
+});
